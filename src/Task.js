5e13cf7c590a0c8d5747abe7ae31ea4e5586165a
alyashofarizqy <96837287+alyashofarizqy@users.noreply.github.com>
2023-06-23 15:19:57 +0700

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://galleryapp-fe4755705.netlify.app/" // TODO: replace this
+const studentName = "Alya Shofarizqi Inayah" // TODO: replace this
+const studentId = "FE4755705" // TODO: replace this
 
 export { url, studentName, studentId }
